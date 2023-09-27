f9ab07c7b4c3473c30077ba936c3c090d1f477ea
jeremykrisna <jeremykrisna@gmail.com>
2023-09-27 23:52:10 +0700

    first commit

---
+++ airflow/dags/modules/covid_scrapper.py
@@ -0,0 +1,15 @@
+import requests
+import pandas as pd
+import logging
+
+class CovidScraper():
+    def __init__(self,url):
+        self.url=url
+    
+    def get_date(self):
+        response=requests.get(self.url)
+        result=response.json()['data']['content']
+        logging.info("GET DATA FROM API COMPLETED")
+        df=pd.json_normalize(result)
+        logging.info("DATA FROM API TO DATAFRAME READY")
+        return df
