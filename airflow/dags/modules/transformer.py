e19437bdafd1e8c50c182ee27f91656a4b43394f
jeremykrisna <jeremykrisna@gmail.com>
2023-10-08 23:02:59 +0700

    revision

--- airflow/dags/modules/transformer.py
+++ airflow/dags/modules/transformer.py
@@ -1,3 +1,5 @@
+import requests
+import json
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -7,57 +9,18 @@ class Transformer:
         self.engine_postgres = engine_postgres
 
     def create_dimension_case(self):
-        query = """SELECT row_number() OVER (ORDER BY closecontact_dikarantina, closecontact_discarded,
-        closecontact_meninggal,confirmation_meninggal,confirmation_sembuh,probable_diisolasi,probable_discarded,
-        probable_meninggal,suspect_diisolasi,suspect_discarded,suspect_meninggal) as id,
-            CASE WHEN closecontact_dikarantina > 0 THEN 'closecontact'
-                WHEN closecontact_discarded > 0 THEN 'closecontact'
-                WHEN closecontact_meninggal > 0 THEN 'closecontact'
-                WHEN confirmation_meninggal > 0 THEN 'confirmation'
-                WHEN confirmation_sembuh > 0 THEN 'confirmation'
-                WHEN probable_diisolasi > 0 THEN 'probable'
-                WHEN probable_discarded > 0 THEN 'probable'
-                WHEN probable_meninggal > 0 THEN 'probable'
-                WHEN suspect_diisolasi > 0 THEN 'suspect'
-                WHEN suspect_discarded > 0 THEN 'suspect'
-                WHEN suspect_meninggal > 0 THEN 'suspect'
-            ELSE NULL
-            END AS status_name,
-            CASE WHEN closecontact_dikarantina > 0 THEN 'dikarantina'
-                WHEN closecontact_discarded > 0 THEN 'discarded'
-                WHEN closecontact_meninggal > 0 THEN 'meninggal'
-                WHEN confirmation_meninggal > 0 THEN 'meninggal'
-                WHEN confirmation_sembuh > 0 THEN 'sembuh'
-                WHEN probable_diisolasi > 0 THEN 'diisolasi'
-                WHEN probable_discarded > 0 THEN 'discarded'
-                WHEN probable_meninggal > 0 THEN 'meninggal'
-                WHEN suspect_diisolasi > 0 THEN 'diisolasi'
-                WHEN suspect_discarded > 0 THEN 'discarded'
-                WHEN suspect_meninggal > 0 THEN 'meninggal'
-            ELSE NULL
-            END AS status_detail,
-            CASE WHEN closecontact_dikarantina > 0 THEN 'closecontact_dikarantina'
-                WHEN closecontact_discarded > 0 THEN 'closecontact_discarded'
-                WHEN closecontact_meninggal > 0 THEN 'closecontact_meninggal'
-                WHEN confirmation_meninggal > 0 THEN 'confirmation_meninggal'
-                WHEN confirmation_sembuh > 0 THEN 'confirmation_sembuh'
-                WHEN probable_diisolasi > 0 THEN 'probable_diisolasi'
-                WHEN probable_discarded > 0 THEN 'probable_discarded'
-                WHEN probable_meninggal > 0 THEN 'probable_meninggal'
-                WHEN suspect_diisolasi > 0 THEN 'suspect_diisolasi'
-                WHEN suspect_discarded > 0 THEN 'suspect_discarded'
-                WHEN suspect_meninggal > 0 THEN 'suspect_meninggal'
-            ELSE NULL
-            END AS status
-            FROM covid_jabar
-    """
+        query="select * from covid_jabar"
+        
         df_case_type = pd.read_sql(query, con=self.engine_sql)
-#        df_case_type = df_case_type.rename(columns={'case_type': 'case_type_name'})
-
-        # Split the status column into status_name and status_detail columns
-#        df_case_type['status_name'] = df_case_type['case_type_id'].str.split('_', 1).str[0]
-#        df_case_type['status_detail'] = df_case_type['case_type_id'].str.split('_', 1).str[1]
-
+        
+        status=[column for column in df_case_type.columns if '_' in column and 'kode' not in column and 'nama' not in column]
+        status_name=[i.rsplit('_')[0] for i in status]
+        status_detail=[i.rsplit('_')[-1] for i in status]
+        id=[i+1 for i in range(len(status))]
+        
+        data={'id': id, 'status_name' : status_name, 'status_detail' : status_detail, 'status' : status}
+        df_case_type=pd.DataFrame(data)
+        
         df_case_type.to_sql('dim_case', con=self.engine_postgres, index=False, if_exists='replace')
 
     def create_dimension_district(self):
@@ -76,6 +39,39 @@ class Transformer:
         df_province_daily.to_sql('province_daily', con=self.engine_postgres, index=False, if_exists='replace')
 
     def create_district_daily(self):
-        query = "SELECT row_number() OVER (ORDER BY kode_kab, tanggal) as id,kode_kab as district_id,tanggal as date, count(1) AS new_case FROM covid_jabar group by kode_kab,tanggal"
+        query="select * from covid_jabar"
+        
         df_district_daily = pd.read_sql(query, con=self.engine_sql)
+        
+        list_case=['closecontact_dikarantina', 'closecontact_discarded',
+        'closecontact_meninggal','confirmation_meninggal','confirmation_sembuh','probable_diisolasi'
+        ,'probable_discarded','probable_meninggal','suspect_diisolasi','suspect_discarded'
+        ,'suspect_meninggal']
+        
+        list_date=[date for date in df_district_daily.tanggal]
+        
+#        sql_date_where = None
+        
+        for case in list_case:
+            for date in list_date:
+                result=self.engine_sql.execute("""
+                                               select sum("+case+") as total 
+                                               from covid_jabar where tanggal = '"+date+"'
+                                               """)
+
+                rows = result.fetchall()
+                if rows:
+                    sql_date_where = rows[0]['total']
+                    print(rows[0]['total'])
+                    
+        kode_kab_sql=[district_id for district_id in df_district_daily.kode_kab]
+        date_sql=[date for date in df_district_daily.tanggal]
+        list_case_sql=[column for column in df_district_daily.columns]
+        
+        id=[i+1 for i in range(len(kode_kab_sql))]
+                   
+        data={'id': id, 'district_id' : kode_kab_sql, 'date' : date_sql , 'total' : sql_date_where}
+        print(data)
+        df_district_daily=pd.DataFrame(data)
+
         df_district_daily.to_sql('district_daily', con=self.engine_postgres, index=False, if_exists='replace')
